533647d4d6487ac30dc7e01b7a62714e5cc72c9a
ronishah <99332468+ronishah@users.noreply.github.com>
2022-02-17 19:09:39 +0530

    Update JUnitHelloWorld.java

--- JUnitHelloWorld/src/main/java/junit/JUnitHelloWorld.java
+++ JUnitHelloWorld/src/main/java/junit/JUnitHelloWorld.java
@@ -5,4 +5,7 @@ public class JUnitHelloWorld {
 	public boolean isGreater(int num1, int num2){
 		return num1 > num2;
 	}
+public static void main (String args[]){
+	System.out.println("Hello Ronil here");
+	}
 }
